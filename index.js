4b3cf5da8816e1fa24c233b5bceeafc2aaa6d5bf
choisuill <gongdal5@naver.com>
2020-10-20 10:32:24 +0900

    2020-10-20 second upload

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var mongoose = require('mongoose');
 var ejs = require('ejs');
 var bodyparser = require('body-parser');
+var methodOverride = require('method-override');
 var app = express();
 
 mongoose.set('useNewUrlParser', true);    // mongoose setting
@@ -11,45 +12,65 @@ mongoose.set('useUnifiedTopology', true); // mongoose setting
 mongoose.connect(process.env.MONGO_DB);
 var db = mongoose.connection;
 db.once('open',function(){
-  console.log('db is connected');
+  console.log('db connected');
 });
 db.on('error',function(err){
   console.log(err);
 });
-app.use(express.static(__dirname + '/public'));   //css setting
-app.set('view engine','ejs');                     //ejs setting
-app.use(bodyparser.json());                       //bodyparser setting
-app.use(bodyparser.urlencoded({extended:true}));  //bodyparser setting
+app.use(express.static(__dirname + '/public'));   // css setting
+app.set('view engine','ejs');                     // ejs setting
+app.use(bodyparser.json());                       // bodyparser setting
+app.use(bodyparser.urlencoded({extended:true}));  // bodyparser setting
+app.use(methodOverride('_method'));               // methodOverride setting
 
-var callbookSchema = mongoose.Schema({
+var callbookSchema = mongoose.Schema({            // Skchema setting
   name: {type:String,unicode:true,required:true},
   call:{type:String,required:true},
   address:{type:String},
   age:{type:String}
 });
-// Skchema setting
-var Callbook = mongoose.model('callbook',callbookSchema);
-// Callbook model Create
+var Callbook = mongoose.model('callbook',callbookSchema);   // Callbook model Create
+// index page
 app.get('/',function(req,res){
   Callbook.find({},function(err,callbooks){
     if(err) return res.json(err);
     res.render('ejsFile',{callbooks:callbooks});
   });
 });
+// show page
 app.get('/ejsFile/show/:id',function(req,res){
     Callbook.findOne({_id:req.params.id},function(err,callbook){
     if(err) return res.json(err);
     res.render('ejsFile/show',{callbook:callbook});
   });
 });
+// create page
 app.get('/ejsFile/create',function(req,res){
   res.render('ejsFile/create');
 });
-
+// create
 app.post('/create',function(req,res){
   Callbook.create(req.body,function(err,callbooks){
     if(err) return res.json(err);
     res.redirect('/');
+  });
+});
+// update page
+app.get('/ejsFile/update/:id',function(req,res){
+  Callbook.findOne({_id:req.params.id},function(err,callbook){
+    res.render('ejsFile/update',{callbook:callbook});
+  });
+});
+// delete
+app.delete('/ejsFile/show/:id',function(req,res){
+  Callbook.deleteOne({_id:req.params.id},function(err){
+    if(err) return res.json(err);
+    res.redirect('/');
+  });
+});
+app.put('/ejsFile/update/:id',function(req,res){
+  Callbook.findOneAndUpdate({_id:req.params.id},req.body,function(err,callbook){
+      res.redirect('/ejsFile/show/'+req.params.id);
   });
 });
 
